fb2dcb941186bf7140fdd5d49d619d7d1876a107
Olrosa <64987055+Olrosa@users.noreply.github.com>
2020-10-21 19:04:04 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,47 +16,46 @@ const modalOn = document.querySelectorAll('[data-modal]'),
     personAdress = document.querySelector('#adress'),
     personPhone = document.querySelector('#phone'),
     personWebsite = document.querySelector('#website'),
-    personCompany = document.querySelector('#company');
+    personCompany = document.querySelector('#company'),
+    user = {};
+
+let valuesUser;
+
+
+
+
 
 // modal
 
+
 modalOn.forEach((btn, i) => {
     btn.addEventListener('click', () => {
-        openModal();
-        fetch(`https://jsonplaceholder.typicode.com/users/${i+1}`)
-            .then(response => response.json())
-            .then(json => {
-                personId.innerHTML = ` ${json.id} `;
-                personName.innerHTML = ` ${json.name} `;
-                personUsername.innerHTML = ` ${json.username} `;
-                personEmail.innerHTML = ` ${json.email} `;
-                personAdress.innerHTML = `street: ${json.address.street}; suite: ${json.address.suite}; city: ${json.address.city} <br>;
-                                            zipcode: ${json.address.zipcode}; geo: lat ${json.address.geo.lat}, lng ${json.address.geo.lng}; `;
-                personPhone.innerHTML = ` ${json.phone} `;
-                personWebsite.innerHTML = ` ${json.website} `;
-                personCompany.innerHTML = ` name: ${json.company.name}, catchPhrase: ${json.company.catchPhrase}, <br>
-                                                bs: ${json.company.bs} `;
-            });
+        addValueInObject(i);
+        openModal();  
+        
+                personId.innerHTML = ` ${valuesUser[0]} `;
+                personName.innerHTML = ` ${valuesUser[1]} `;
+                personUsername.innerHTML = ` ${valuesUser[2]} `;
+                personEmail.innerHTML = ` ${valuesUser[3]} `;
+                personAdress.innerHTML = ` ${valuesUser[4]} `;
+                personPhone.innerHTML = ` ${valuesUser[5]} `;
+                personWebsite.innerHTML = ` ${valuesUser[6]} `;
+                personCompany.innerHTML = ` ${valuesUser[7]} `;
+            
     });
 });
 
 document.addEventListener('keydown', (e) => {
     if (e.code === "Escape" && modal.classList.contains('modal-on')) {
         closeModal();
-        personId.innerHTML = "";
-        personName.innerHTML = "";
-        personUsername.innerHTML = "";
-        personEmail.innerHTML = "";
-        personAdress.innerHTML = "";
-        personPhone.innerHTML = "";
-        personWebsite.innerHTML = "";
-        personCompany.innerHTML = "";
+
     }
 });
 
 addName();
 
 // functions
+
 
 function addName() {
     name.forEach((btn, i) => {
@@ -79,3 +78,25 @@ function openModal() {
     modal.classList.remove('modal-off');
 
 }
+
+function addValueInObject(i) {
+    fetch(`https://jsonplaceholder.typicode.com/users/${i+1}`)
+            .then(response => response.json())
+            .then(json => {
+                user.personId = `${json.id}`;
+                
+                user.personName = ` ${json.name} `;
+                user.personUsername = ` ${json.username} `;
+                user.personEmail = ` ${json.email} `;
+                user.personAdress = `street: ${json.address.street}; suite: ${json.address.suite}; city: ${json.address.city} <br>;
+                                            zipcode: ${json.address.zipcode}; geo: lat ${json.address.geo.lat}, lng ${json.address.geo.lng}; `;
+                user.personPhone= ` ${json.phone} `;
+                user.personWebsite = ` ${json.website} `;
+                user.personCompany = ` name: ${json.company.name}, catchPhrase: ${json.company.catchPhrase}, <br>
+                                                bs: ${json.company.bs} `;
+            });
+    valuesUser = Object.values(user);
+}
+
+
+
